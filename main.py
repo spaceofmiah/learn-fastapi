393b33f6e7dca9d4449fa770e50e51f5e8566bd6
spaceofiah <agbonzeosazuwa@gmail.com>
2023-01-11 11:51:41 +0100

    Add signup endpoint

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import fastapi
+from fastapi import Body, Depends
+from sqlalchemy.orm import Session
+
+from db_initializer import get_db
+from models import users as user_model
+from schemas.users import CreateUserSchema, UserSchema
+from services.db import users as user_db_services
 
 
 app = fastapi.FastAPI()
@@ -17,3 +24,13 @@ def login():
 	- password:
 	"""
 	return "ThisTokenIsFake"
+
+
+@app.post('/signup', response_model=UserSchema)
+def signup(
+	payload: CreateUserSchema = Body(), 
+	session:Session=Depends(get_db)
+):
+	"""Processes request to register user account."""
+	payload.hashed_password = user_model.User.hash_password(payload.hashed_password)
+	return user_db_services.create_user(session, user=payload)
