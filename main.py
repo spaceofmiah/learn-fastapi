8c724f9082a295d5b3f90a30a4e19af696c43d64
spaceofiah <agbonzeosazuwa@gmail.com>
2023-01-11 21:23:04 +0100

    Refactor login endpoint

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
+from typing import Dict
+
 import fastapi
-from fastapi import Body, Depends
+from fastapi import Body, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from db_initializer import get_db
 from models import users as user_model
-from schemas.users import CreateUserSchema, UserSchema
 from services.db import users as user_db_services
+from schemas.users import CreateUserSchema, UserSchema, UserLoginSchema
 
 
 app = fastapi.FastAPI()
 
 
-@app.post('/login')
-def login():
+@app.post('/login', response_model=Dict)
+def login(
+		payload: UserLoginSchema = Body(),
+		session: Session = Depends(get_db)
+	):
 	"""Processes user's authentication and returns a token
 	on successful authentication.
 
@@ -23,7 +28,25 @@ def login():
 
 	- password:
 	"""
-	return "ThisTokenIsFake"
+	try:
+		user:user_model.User = user_db_services.get_user(
+			session=session, email=payload.email
+		)
+	except:
+		raise HTTPException(
+			status_code=status.HTTP_401_UNAUTHORIZED,
+			detail="Invalid user credentials"
+		)
+
+	is_validated:bool = user.validate_password(payload.password)
+	if not is_validated:
+		raise HTTPException(
+			status_code=status.HTTP_401_UNAUTHORIZED,
+			detail="Invalid user credentials"
+		)
+
+	return user.generate_token()
+
 
 
 @app.post('/signup', response_model=UserSchema)
